b5600f864ad46335eacf1071044ead5967d68b14
DavidPT <davidpt94@gmail.com>
2024-06-05 13:14:21 -0500

    change

--- src/main/java/com/riwi/filtro/api/dto/request/LessonReq.java
+++ src/main/java/com/riwi/filtro/api/dto/request/LessonReq.java
@@ -1,10 +1,6 @@
 package com.riwi.filtro.api.dto.request;
 
-import java.time.LocalDateTime;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
